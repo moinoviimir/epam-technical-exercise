c1ec2bcf7289a1a9c42e76f059988208fb73ccfc
Mir <moinoviimir@gmail.com>
2016-01-28 08:07:58 +0300

    Updated to enable providing the sentence through the command line
    Seemed appropriate, as the acceptance criteria could be understood as
    favouring this approach as opposed to manual input

--- WordCounter.App/Program.cs
+++ WordCounter.App/Program.cs
@@ -6,10 +6,21 @@ namespace WordCounter.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the sentence");
-            var sentence = Console.ReadLine();
+            string sentence = String.Empty;
+            if (args.Length != 0)
+            {
+                sentence = args[0];
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the sentence");
+                sentence = Console.ReadLine();
+            }
+
             var result = new WordCounter(sentence).CountWords();
 
+            Console.WriteLine();
             foreach (var key in result.Keys)
             {
                 Console.WriteLine("{0} - {1}", key, result[key]);
